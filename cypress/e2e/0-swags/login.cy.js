5d77c2613c3a31c22b45f342e2199590cc6d25de
lansylord <lansylord@gmail.com>
2026-05-04 20:54:51 -0300

    feat: Adiciona novos casos de teste de login

--- cypress/e2e/0-swags/login.cy.js
+++ cypress/e2e/0-swags/login.cy.js
@@ -8,6 +8,14 @@ context('Logins', () => {
         cy.get('#user-name').type('standard_user')
         cy.get('#password').type('secret_sauce')
         cy.get('#login-button').click()
+        cy.get('[data-test="title"]').should('have.text', 'Products');
+    })
+
+    it('Username inválido', () => {
+        cy.get('#user-name').type('usuario_padrao')
+        cy.get('#password').type('secret_sauce')
+        cy.get('#login-button').click()
+        cy.get('[data-test="error"]').should('have.text', 'Epic sadface: Username and password do not match any user in this service');
     })
 
 })
